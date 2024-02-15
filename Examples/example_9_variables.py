c5774007ba0df2fe0966206d96c4813189bb1a4c
ariannedee <ariannedee@gmail.com>
2024-02-15 10:39:22 -0800

    Fill out variables example

---
+++ Examples/example_9_variables.py
@@ -0,0 +1,24 @@
+def print_id(obj):
+    print(f"""ID of {obj}:
+    {id(obj)}
+    {hex(id(obj))}
+    {super(type(obj), obj).__repr__()}
+    """)
+
+
+# These are the same because they refer to the same object
+x = 123
+y = x
+print_id(x)
+print_id(y)
+
+# These are different because x refers to a new object
+# Same if object is interred or immortal (-5 <= x <= 255
+x += 1
+print_id(x)
+print_id(y)
+
+# These may or may not be the same depending on the initial value of x
+y += 1
+print_id(x)
+print_id(y)
