dca4e3d3c78f789f8039abef3c01b9eb64d964d2
ariannedee <ariannedee@gmail.com>
2024-02-23 11:53:05 -0800

    Add copy examples

---
+++ Examples/example_12_copy.py
@@ -0,0 +1,13 @@
+import copy
+
+a = [[]]
+b = a                 # References same object as a
+c = copy.copy(a)      # Shallow copy - id(c) != id(a) but contents are the same objects
+d = copy.deepcopy(a)  # Deep copy - all contents are new objects
+
+a.append(1)
+a[0].append(2)
+print(a)  # [[2], 1]
+print(b)  # [[2], 1]
+print(c)  # [[2]]
+print(d)  # [[]]
