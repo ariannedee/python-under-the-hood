355461f77e3ce97f1bfa5dac24b62fbe3c6c5d5b
ariannedee <ariannedee@gmail.com>
2024-02-26 13:36:55 -0800

    Add basic methods example

--- Examples/example_19_basic_methods.py
+++ Examples/example_19_basic_methods.py
@@ -9,28 +9,42 @@ class Book:
         obj = super().__new__(cls)
         return obj
 
-    def __init__(self, name, author):
-        self.name = name
+    def __init__(self, title, author):
+        self.title = title
         self.author = author
-        print(f'__init__ called with {name}, {author}')
+        print(f'__init__ called with {title}, {author}')
 
     def __del__(self):
         print(f"{self} destroyed")
 
     def __str__(self):
-        return f"{self.name} by {self.author}"
+        return f"{self.title} by {self.author}"
 
     def __repr__(self):
-        return f"Book({repr(self.name)}, {repr(self.author)})"
+        return f"Book({repr(self.title)}, {repr(self.author)})"
 
     def __eq__(self, other):
         if isinstance(other, Book):
-            return self.name == other.name and self.author == other.author
+            return self.title == other.title and self.author == other.author
         return False
 
     def __bool__(self):
         """Returns True if no values are empty strings or None"""
-        return bool(self.name) and bool(self.author)
+        return bool(self.title) and bool(self.author)
+
+    def __hash__(self):
+        return hash((self.title, self.author))
+
+    def __format__(self, format_spec):
+        if not format_spec:
+            return str(self)
+        elif format_spec == 'u':
+            return str(self).upper()
+        elif format_spec == 't':
+            return self.title
+        elif format_spec == 'a':
+            return self.author
+        raise ValueError(f"Invalid format specifier {format_spec} for object of type 'Book'")
 
 
 if __name__ == '__main__':
@@ -50,7 +64,7 @@ if __name__ == '__main__':
     if book1 == book2:  # __eq__ called
         print("Same book")
 
-    book2.name = 'The Iliad'
+    book2.title = 'The Iliad'
     if book1 != book2:  # __eq__ called even though __neq__ not implemented
         print("Different book")
 
@@ -58,6 +72,14 @@ if __name__ == '__main__':
         book1 > book2
     except TypeError as e:
         print(repr(e))
+
+    num_reads = {
+        book1: 1,
+        book2: 2,
+    }
+    print(num_reads)  # book1 == book2 and hash(book1) == hash(book2) so only 1 entry made
+
+    print(f"I LOVED {book1:u}")  # Calls __format__ with 'u'
 
     del book1
     print('Book 1 not destroyed yet because of reference in books')
