12d67750bfbc2c602cbb0365fce1c47f3a4d0ed9
ariannedee <ariannedee@gmail.com>
2026-03-09 14:22:25 -0700

    Use built-in generics

--- Examples/example_15_typehints.py
+++ Examples/example_15_typehints.py
@@ -1,21 +1,18 @@
 """Make use of type hints for warnings and code completion in your IDE"""
+from __future__ import annotations
+
 import requests
-
-
-from typing import List, Tuple
-
-from requests import Response
 
 URL = "https://nominatim.openstreetmap.org/reverse"
 
-Coordinates = Tuple[float, float]
+Coordinates = tuple[float, float]
 
 
 # Function type annotations
 def get_location_name(coords: Coordinates) -> tuple:
     params = {'lat': coords[0], 'lon': coords[1], 'format': 'json', 'zoom': 12}
     headers = {'User-Agent': f'Python script by <insert name or email here>'}
-    response: Response = requests.get(URL, params, headers=headers)
+    response: requests.Response = requests.get(URL, params, headers=headers)
     response.raise_for_status()
     location_details: dict = response.json()
     address: dict = location_details['address']
@@ -29,12 +26,12 @@ dublin: tuple[float, float] = (40.10, -83.11)
 print(get_location_name(vancouver))  # PyCharm shows type mismatch because tuple != tuple[float, float]
 
 # Using types from typing module
-london: Tuple[float, float] = (42.99, -81.25)
-cities: List[Tuple[float, float]] = [vancouver, dublin]
+london: tuple[float, float] = (42.99, -81.25)
+cities: list[tuple[float, float]] = [vancouver, dublin]
 
 # Using type aliases (Coordinates defined at top of file)
 brooklyn: Coordinates = (-37.816, 144.842)
-more_cities: List[Coordinates]
+more_cities: list[Coordinates]
 more_cities = [dublin, london, brooklyn]
 
 for city in more_cities:
